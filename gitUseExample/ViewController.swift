c1b61174ece3172c3febdb287cdf75d68adc1d3e
Sulman Riaz <sulman-1@hotmail.com>
2015-10-29 19:41:34 -0400

    same thing again

--- gitUseExample/ViewController.swift
+++ gitUseExample/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         print("Tryin to commit tis")
+        print("this is a nother change")
+        
     }
 
     override func didReceiveMemoryWarning() {
